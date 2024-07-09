e6183558a291a4588ba9c278c16245dd5640488d
Adam <adamo@Adams-MacBook-Pro.local>
2024-07-09 07:19:00 +0200

    init

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
-package k6ext1
+package ext1
 
-import "fmt"
+import (
+	"fmt"
 
-func K6ext1() {
-	fmt.Println("k6-ext1 ext")
+	"go.k6.io/k6/js/modules"
+)
+
+// init is called by the Go runtime at application startup.
+func init() {
+	modules.Register("k6/x/compare", new(Ext1))
+}
+
+// Compare is the type for our custom API.
+type Ext1 struct {
+	ComparisonResult string // textual description of the most recent comparison
+}
+
+// IsGreater returns true if a is greater than b, or false otherwise, setting textual result message.
+func (c *Ext1) IsGreater(a, b int) bool {
+	if a > b {
+		c.ComparisonResult = fmt.Sprintf("%d is greater than %d", a, b)
+		return true
+	} else {
+		c.ComparisonResult = fmt.Sprintf("%d is NOT greater than %d", a, b)
+		return false
+	}
 }
